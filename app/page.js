e07cac25e86bcd6d0d18864efb2b5021dc8e7f93
Rishi Khare <rishiskhare@gmail.com>
2023-06-17 15:14:48 -0700

    Changed landing page

--- app/page.js
+++ app/page.js
@@ -1,16 +1,15 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl">Flip Flop</h1>
+    <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
+      <div className="flex flex-col items-center justify-evenly">
+        <h1 className="text-5xl p-4">What Should I Eat?</h1>
+        <h2 className="text-2xl">Feeling hungry?</h2>
+      </div>
       <div className="flex p-2 space-x-4">
-        <Link className="rounded p-2 bg-purple-300" href="/buy">
-          Buy
-        </Link>
-        <Link className="rounded p-2 bg-purple-300" href="/sell">
-          Sell
+        <Link className="text-blue-500 rounded p-2" href="/buy">
+          Continue to help choose a restaurant
         </Link>
       </div>
     </main>
--- app/sell/page.js
+++
@@ -1,5 +0,0 @@
-const Sell = () => {
-  return <h1>Sell</h1>;
-};
-
-export default Sell;
