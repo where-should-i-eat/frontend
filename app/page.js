9c0b4e2a7bfa4651324e266a2efd0e95ad813671
Rishi Khare <rishiskhare@gmail.com>
2023-06-17 11:44:10 -0700

    Added rudimentary home styling

--- app/page.js
+++ app/page.js
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home page</h1>
+      <h1 className="text-5xl">Flip Flop</h1>
+      <div className="flex p-2 space-x-4">
+        <Link className="rounded p-2 bg-purple-300" href="/buy">
+          Buy
+        </Link>
+        <Link className="rounded p-2 bg-purple-300" href="/sell">
+          Sell
+        </Link>
+      </div>
     </main>
   );
 }
