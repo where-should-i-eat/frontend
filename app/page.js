65b7029fa81f8dff445513a8375f1fcad08a68a3
Rishi Khare <rishiskhare@gmail.com>
2023-06-17 15:21:20 -0700

    Changed app name on landing page/Navbar

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
       <div className="flex flex-col items-center justify-evenly">
-        <h1 className="text-5xl p-4">What Should I Eat?</h1>
+        <h1 className="text-5xl p-4">Where Should I Eat?</h1>
         <h2 className="text-2xl">Feeling hungry?</h2>
       </div>
       <div className="flex p-2 space-x-4">
