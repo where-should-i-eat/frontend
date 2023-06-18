068e2f991424d5bd114948f1a0bc39c5326d90fb
Rishi Khare <rishiskhare@gmail.com>
2023-06-17 18:57:51 -0700

    Changed route in Navbar

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { FC } from "react";
+import Link from "next/link";
 
 export const Navbar: FC = () => {
   return (
     <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
       <div className="font-bold text-3xl flex items-center">
-        <a
-          className="ml-2 hover:opacity-50"
-          href="https://code-scaffold.vercel.app"
-        >
+        <Link className="ml-2 hover:opacity-50" href="/">
           Where Should I Eat?
-        </a>
+        </Link>
       </div>
     </div>
   );
