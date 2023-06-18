28f095824c1603b8d564e7a2a7dd37827dcef336
Depetrol <lslrobert@outlook.com>
2023-06-17 21:02:37 -0700

    fix a few pixels

--- app/buy/page.js
+++ app/buy/page.js
@@ -1,18 +1,18 @@
-"use client";
-import { Chat } from "@/components/Chat/Chat";
-import { ChatInput } from "@/components/Chat/ChatInput";
-import { ResetChat } from "@/components/Chat/ResetChat";
-import { ChatMessage } from "@/components/Chat/ChatMessage";
-import { ChatLoader } from "@/components/Chat/ChatLoader";
+'use client';
+import { Chat } from '@/components/Chat/Chat';
+import { ChatInput } from '@/components/Chat/ChatInput';
+import { ResetChat } from '@/components/Chat/ResetChat';
+import { ChatMessage } from '@/components/Chat/ChatMessage';
+import { ChatLoader } from '@/components/Chat/ChatLoader';
 
-import { Footer } from "@/components/Layout/Footer";
-import { Navbar } from "@/components/Layout/Navbar";
-import { Message } from "@/types";
-import Head from "next/head";
-import NoSSR from "react-no-ssr";
-import { useEffect, useRef, useState } from "react";
-import axios from "axios";
-import Link from "next/link";
+import { Footer } from '@/components/Layout/Footer';
+import { Navbar } from '@/components/Layout/Navbar';
+import { Message } from '@/types';
+import Head from 'next/head';
+import NoSSR from 'react-no-ssr';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import Link from 'next/link';
 
 export default function Home() {
   const [messages, setMessages] = useState([]);
@@ -22,7 +22,7 @@ export default function Home() {
   const messagesEndRef = useRef(null);
 
   const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
   const handleSend = async (message) => {
@@ -33,48 +33,48 @@ export default function Home() {
 
     const backendUrl = process.env.BACKEND_URL;
 
-    console.log("Sending to ", backendUrl);
+    console.log('Sending to ', backendUrl);
     const response = await axios.post(
       `${backendUrl}/api/chat`,
       { messages: updatedMessages, location: location },
       {
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       }
     );
 
     if (response.status != 200) {
       setLoading(false);
-      console.log("RESPONSE FAILED:", response);
+      console.log('RESPONSE FAILED:', response);
       throw new Error(response.statusText);
     }
 
-    console.log("RESPONSE SUCCESSFUL", response);
+    console.log('RESPONSE SUCCESSFUL', response);
     const data = response.data;
     if (!data) {
       return;
     }
 
     setLoading(false);
-    setMessages(data["messages"]);
+    setMessages(data['messages']);
   };
 
   const handleReset = () => {
     setMessages([
       {
-        role: "assistant",
+        role: 'assistant',
         content: `Hello! I am a helpful AI assistant designed to help you choose a restaurant. To begin, what type of cuisine you are in the mood for?`,
       },
     ]);
   };
 
   function getGeoLocation() {
-    console.log("Getting Location");
+    console.log('Getting Location');
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(success, error);
     } else {
-      console.log("Geolocation not supported");
+      console.log('Geolocation not supported');
     }
     function success(position) {
       const latitude = position.coords.latitude;
@@ -83,7 +83,7 @@ export default function Home() {
       console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
     }
     function error() {
-      console.log("Unable to retrieve your location");
+      console.log('Unable to retrieve your location');
     }
   }
 
@@ -98,7 +98,7 @@ export default function Home() {
   useEffect(() => {
     setMessages([
       {
-        role: "assistant",
+        role: 'assistant',
         content: `Hello! I am a helpful AI assistant designed to help you choose a restaurant. To begin, what type of cuisine you are in the mood for?`,
       },
       // {
@@ -141,7 +141,7 @@ export default function Home() {
         </Head>
 
         <div className="flex flex-col h-screen">
-          <div className="flex h-[60px] border-b border-neutral-300 py-4 px-4 sm:px-8 items-center justify-between">
+          <div className="flex h-[50px] border-b border-neutral-300 py-4 px-4 sm:px-8 items-center justify-between">
             <div className="font-bold text-3xl flex items-center">
               <Link className="ml-2 hover:opacity-50" href="/">
                 Where Should I Eat?
@@ -170,7 +170,7 @@ export default function Home() {
             </div>
             <Footer />
           </div>
-          <div className="flex h-[30px] py-2 px-8 items-center justify-center">
+          <div className="flex h-[50px] py-2 px-8 items-center justify-center">
             <div className="mt-2 w-full lg:w-2/3">
               <ChatInput onSend={handleSend} />
             </div>
