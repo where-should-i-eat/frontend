8a917598a25f3709591abfd5b3d00c31e458d1c8
Rishi Khare <rishiskhare@gmail.com>
2023-06-17 19:05:33 -0700

    Changed initial prompting for frontend

--- app/buy/page.js
+++ app/buy/page.js
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 import { Chat } from "@/components/Chat/Chat";
 import { Footer } from "@/components/Layout/Footer";
 import { Navbar } from "@/components/Layout/Navbar";
 import { Message } from "@/types";
 import Head from "next/head";
-import NoSSR from 'react-no-ssr';
+import NoSSR from "react-no-ssr";
 import { useEffect, useRef, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 export default function Home() {
   const [messages, setMessages] = useState([]);
@@ -26,23 +26,24 @@ export default function Home() {
 
     const backendUrl = process.env.BACKEND_URL;
 
-    console.log("Sending to ", backendUrl)
-    const response = await axios.post(`${backendUrl}/api/chat`, 
-      {messages: updatedMessages},
+    console.log("Sending to ", backendUrl);
+    const response = await axios.post(
+      `${backendUrl}/api/chat`,
+      { messages: updatedMessages },
       {
         headers: {
-          'Content-Type': 'application/json'
-        }
+          "Content-Type": "application/json",
+        },
       }
-      )
+    );
 
-    if (response.status!=200) {
+    if (response.status != 200) {
       setLoading(false);
-      console.log("RESPONSE FAILED:", response)
+      console.log("RESPONSE FAILED:", response);
       throw new Error(response.statusText);
     }
 
-    console.log("RESPONSE SUCCESSFUL", response)
+    console.log("RESPONSE SUCCESSFUL", response);
     const data = response.data;
     if (!data) {
       return;
@@ -56,8 +57,8 @@ export default function Home() {
     setMessages([
       {
         role: "assistant",
-        content: `Hi there! I'm Chatbot UI, an AI assistant. I can help you with things like answering questions, providing information, and helping with tasks. How can I help you?`
-      }
+        content: `Hello! I am a helpful AI assistant designed to help you choose a restaurant. To begin, what type of cuisine you are in the mood for?`,
+      },
     ]);
   };
 
@@ -69,64 +70,64 @@ export default function Home() {
     setMessages([
       {
         role: "assistant",
-        content: `Hi there! I'm Chatbot UI, an AI assistant. I can help you with things like answering questions, providing information, and helping with tasks. How can I help you?`
+        content: `Hello! I am a helpful AI assistant designed to help you choose a restaurant. To begin, what type of cuisine you are in the mood for?`,
       },
       {
         role: "assistant-image",
-        content: `https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1920px-Image_created_with_a_mobile_phone.png`
+        content: `https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1920px-Image_created_with_a_mobile_phone.png`,
       },
       {
         role: "assistant-images",
-        content: [`https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1920px-Image_created_with_a_mobile_phone.png`, `https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1920px-Image_created_with_a_mobile_phone.png`, `https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1920px-Image_created_with_a_mobile_phone.png`]
+        content: [
+          `https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1920px-Image_created_with_a_mobile_phone.png`,
+          `https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1920px-Image_created_with_a_mobile_phone.png`,
+          `https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1920px-Image_created_with_a_mobile_phone.png`,
+        ],
       },
       {
         role: "assistant-map",
         content: {
-          center: {lat: 40.73, lng: -73.93}, 
+          center: { lat: 40.73, lng: -73.93 },
           zoom: 12,
-          markers: [ {lat: 40.73, lng: -73.93, title: "This is a Marker"}, 
-              {lat: 40.63, lng: -73.89, title: "This is Marker 2"}]
-        }
+          markers: [
+            { lat: 40.73, lng: -73.93, title: "This is a Marker" },
+            { lat: 40.63, lng: -73.89, title: "This is Marker 2" },
+          ],
+        },
       },
     ]);
   }, []);
 
   return (
     <>
-    <NoSSR>
-      <Head>
-        <title>Chatbot UI</title>
-        <meta
-          name="description"
-          content="A simple chatbot starter kit for OpenAI's chat model using Next.js, TypeScript, and Tailwind CSS."
-        />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        />
-        <link
-          rel="icon"
-          href="/favicon.ico"
-        />
-      </Head>
+      <NoSSR>
+        <Head>
+          <title>Chatbot UI</title>
+          <meta
+            name="description"
+            content="A simple chatbot starter kit for OpenAI's chat model using Next.js, TypeScript, and Tailwind CSS."
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <div className="flex flex-col h-screen">
-        <Navbar />
+        <div className="flex flex-col h-screen">
+          <Navbar />
 
-        <div className="flex-1 overflow-auto sm:px-10 pb-4 sm:pb-10">
-          <div className="max-w-[900px] mx-auto mt-4 sm:mt-12">
-            <Chat
-              messages={messages}
-              loading={loading}
-              onSend={handleSend}
-              onReset={handleReset}
-            />
-            <div ref={messagesEndRef} />
+          <div className="flex-1 overflow-auto sm:px-10 pb-4 sm:pb-10">
+            <div className="max-w-[900px] mx-auto mt-4 sm:mt-12">
+              <Chat
+                messages={messages}
+                loading={loading}
+                onSend={handleSend}
+                onReset={handleReset}
+              />
+              <div ref={messagesEndRef} />
+            </div>
           </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </NoSSR>
+      </NoSSR>
     </>
   );
 }
